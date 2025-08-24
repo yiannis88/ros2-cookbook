388fb94ad7a388bc039caeee20017f21259f04cb
yiannis88 <selinis.g@gmail.com>
2025-08-24 23:55:41 +0100

    update licence

--- examples/hello_pubsub/hello_pubsub_py/setup.py
+++ examples/hello_pubsub/hello_pubsub_py/setup.py
@@ -4,7 +4,7 @@ package_name = 'hello_pubsub_py'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.0.1',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -13,10 +13,10 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='yanis',
+    maintainer='Ioannis Selinis',
     maintainer_email='selinis.g@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='Minimal (Hello) ROS2 publisher & subscriber nodes',
+    license='MIT',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
